6def13d1b9264e6a9ed5f0d2e2a0aa4526fab334
Osman ÖZBEK <77728642+ozbek2o@users.noreply.github.com>
2021-11-29 00:48:46 +0300

    Add files via upload

---
+++ metot-uyg/mainnnnn/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainnnnn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            yenidenişlemyap:
+        matematikselişlemler m = new matematikselişlemler();
+            m.menuhazırla();
+            int kulanıcısecim = int.Parse(Console.ReadLine());
+
+            Console.WriteLine( "lütfen birinci sayı değerini giriniz");
+            decimal kulanıcısayı1 = decimal.Parse(Console.ReadLine());
+            
+            Console.WriteLine(" lütfen ikinci sayı değerini giriniz ");
+            decimal kulanıcısayı2 = decimal.Parse(Console.ReadLine());
+
+            decimal sonuc = 0;
+            switch(kulanıcısecim)
+                {
+                case 1://topla
+                    sonuc = m.toplamaıslemi(kulanıcısayı1, kulanıcısayı2);
+                    m.sonucekranayaz(kulanıcısayı1, kulanıcısayı2, sonuc, "+");
+                    break;
+                case 2://çıkar
+                    sonuc = m.bölmeıslemi(kulanıcısayı1, kulanıcısayı2);
+                    m.sonucekranayaz(kulanıcısayı1, kulanıcısayı2, sonuc, "-");
+                    break;
+                case 3://çarp
+                    sonuc = m.çarpmaıslemi(kulanıcısayı1, kulanıcısayı2);
+                    m.sonucekranayaz(kulanıcısayı1, kulanıcısayı2, sonuc, "*");
+                    break;
+                case 4://bölme
+                    sonuc = m.bölmeıslemi(kulanıcısayı1, kulanıcısayı2);
+                    m.sonucekranayaz(kulanıcısayı1, kulanıcısayı2, sonuc, "/");
+                    break;
+                default:
+                    Console.WriteLine("Belirtmiş olduğunuz değer liste içerisinde bulunmamaktadır");
+                    Console.WriteLine("lütfen yeniden deneyiniz");
+                    System.Threading.Thread.Sleep(2000);
+                    goto yenidenişlemyap;
+            }
+            Console.WriteLine("yeniden işlem yapmak istiyormusunuz? [E/H]");
+            string EH = Console.ReadLine();
+            if(EH=="E" || EH=="e")
+                {
+                goto yenidenişlemyap;
+            }
+
+        }         
+    }
+}
---
+++ metot-uyg/mainnnnn/matematikselişlemler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainnnnn
+{
+    public class matematikselişlemler
+    {
+        //topla
+        public decimal toplamaıslemi(decimal sy1, decimal sy2)
+        {
+            decimal sonuc = sy1 + sy2;
+            return sonuc;
+        }
+        //çıkar
+        public decimal çıkarmaıslemi(decimal sy1,  decimal sy2)
+        {
+            decimal sonuc = sy1 - sy2;
+            return sonuc;
+        }
+
+        //çarp
+        public decimal çarpmaıslemi(decimal sy1 , decimal sy2)
+        {
+            decimal sonuc = sy1 * sy2;
+            return sonuc;
+        }
+
+        //bölme
+        public decimal bölmeıslemi (decimal sy1 ,decimal sy2)
+        {
+            decimal sonuc = sy1 / sy2;
+            return sonuc;
+        }
+
+        public void menuhazırla ()
+        {
+            Console.Clear();
+            Console.WriteLine("****MENU****");
+            Console.WriteLine("1-TOPLAMA");
+            Console.WriteLine("2-ÇIKARMA");
+            Console.WriteLine("3-ÇARPMA");
+            Console.WriteLine("4-BÖLME");
+            Console.Write("LÜTFEN DEĞER SEÇİNİZ:");
+        }
+
+        public void sonucekranayaz(decimal kulanıcısayı1, decimal kulanıcısayı2, decimal sonuc ,string operators)
+        {
+            Console.WriteLine("{0} {1} {2} ={3}" ,kulanıcısayı1 , operators, kulanıcısayı2, sonuc);
+        }
+      }
+}
